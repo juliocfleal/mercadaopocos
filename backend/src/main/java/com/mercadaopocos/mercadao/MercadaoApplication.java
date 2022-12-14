2a7325502508a0335be53c3f5fc485b13ef5dd1c
Julio Cesar Freitas Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2022-12-13 23:42:08 -0300

    First commit

---
+++ backend/src/main/java/com/mercadaopocos/mercadao/MercadaoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadaopocos.mercadao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mercadaopocos/mercadao/MercadaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadaopocos.mercadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
